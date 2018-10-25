ef736b78458cfa34e89e3ec8fc54b36d123ef25a
Jacques Jacob <jacques.jacob@gmail.com>
2018-10-24 23:41:32 -0500

    new pubg season status and rank

--- pubgteste.py
+++ pubgteste.py
@@ -27,11 +27,11 @@ async def on_message(message):
         js = response.json()
         js2 = js['data'][0]
         js3 = js2['id']
-        link = "https://api.pubg.com/shards/pc-sa/seasons"
+        link = "https://api.pubg.com/shards/steam/seasons"
         reponse2 = requests.get(url=link, headers=header)
         jso = reponse2.json()
         jso2 = jso['data'][0]['id']
-        link2 = 'https://api.pubg.com/shards/pc-sa/players/{}/seasons/division.bro.official.2018-09'.format(js3)
+        link2 = 'https://api.pubg.com/shards/steam/players/{}/seasons/division.bro.official.pc-2018-01'.format(js3)
         response3 = requests.get(link2, headers=header)
         json1 = response3.json()
         json2 = json1['data']['attributes']['gameModeStats']
@@ -39,49 +39,111 @@ async def on_message(message):
         json4 = json2['duo']
         json5 = json2['squad']
 
-        PontosSolo = json3['winPoints']
-        PontosDuo = json4['winPoints']
-        PontosSquad = json5['winPoints']
+#        PontosSolo = json3['winPoints']
+#        PontosDuo = json4['winPoints']
+#        PontosSquad = json5['winPoints']
 
-        KillPSolo = json3['killPoints']
-        KillPDuo = json4['killPoints']
-        KillPSquad = json5['killPoints']
+#        KillPSolo = json3['killPoints']
+#        KillPDuo = json4['killPoints']
+#        KillPSquad = json5['killPoints']
 
-        PSolo = KillPSolo * 0.2 + PontosSolo
-        PSolo4 = int(PSolo)
-        PDuo = KillPDuo * 0.2 + PontosDuo
-        PDuo4 = int(PDuo)
-        PSquad = KillPSquad * 0.2 + PontosSquad
-        PSquad4 = int(PSquad)
+        rankPSolo = json3['rankPoints']
+        rankPSolo = int(rankPSolo)
+        rankPDuo = json4['rankPoints']
+        rankPDuo = int(rankPDuo)
+        rankPSquad = json5['rankPoints']
+        rankPSquad = int(rankPSquad)
 
-        bronze = ':third_place:'
-        prata = ':second_place:'
-        ouro = ':first_place:'
+#        PSolo = KillPSolo * 0.2 + PontosSolo
+#        PSolo4 = int(PSolo)
+#        PDuo = KillPDuo * 0.2 + PontosDuo
+#        PDuo4 = int(PDuo)
+#        PSquad = KillPSquad * 0.2 + PontosSquad
+#        PSquad4 = int(PSquad)
 
-        if PSolo <= 1780:
-            Solo = bronze
-        elif PSolo >= 2000:
-            Solo = ouro
+        bronze = ':third_place: (bronze)'
+        prata = ':second_place: (prata)'
+        ouro = ':first_place: (ouro)'
+        platinum = ':military_medal: (platinum)'
+        diamond = ':gem: (diamond)'
+        elite = ':medal: (elite)'
+        master = ':medal: :medal: (master)'
+        grandmaster = ':trophy: (grandmaster)'
+
+        SoloPlayed = json3['roundsPlayed']
+        DuoPlayed = json4['roundsPlayed']
+        SquadPlayed = json5['roundsPlayed']
+
+        MPointsSolo = int(json3['bestRankPoint'])
+        MPointsDuo = int(json4['bestRankPoint'])
+        MPointsSquad = int(json5['bestRankPoint'])
+
+        if SoloPlayed <= 9:
+            rankPSolo = 0
+            Solo = '(menos de 10 partidas)'
+        elif rankPSolo <= 1399:
+                Solo = bronze
+        elif rankPSolo >= 1500 and rankPSolo <= 1599:
+                Solo = ouro
+        elif rankPSolo >= 1400 and rankPSolo <= 1499:
+                Solo = prata
+        elif rankPSolo >= 1600 and rankPSolo <= 1699:
+                Solo = platinum
+        elif rankPSolo >= 1700 and rankPSolo <= 1799:
+                Solo = diamond
+        elif rankPSolo >= 1800 and rankPSolo <= 1899:
+                Solo = elite
+        elif rankPSolo >= 1900 and rankPSolo <= 1999:
+                Solo = master
         else:
-            Solo = prata
+                Solo = grandmaster
 
-        if PDuo <= 1780:
+
+        if DuoPlayed <= 9:
+            rankPDuo = 0
+            Duo = '(menos de 10 partidas)'
+        elif rankPDuo <= 1399:
             Duo = bronze
-        elif PDuo >= 2000:
+        elif rankPDuo >= 1500 and rankPDuo <= 1599:
             Duo = ouro
-        else:
+        elif rankPDuo >= 1400 and rankPDuo <= 1499:
             Duo = prata
-
-        if PSquad <= 1780:
-            Squad = bronze
-        elif PSquad >= 2000:
-            Squad = ouro
+        elif rankPDuo >= 1600 and rankPDuo <= 1699:
+            Duo = platinum
+        elif rankPDuo >= 1700 and rankPDuo <= 1799:
+            Duo = diamond
+        elif rankPDuo >= 1800 and rankPDuo <= 1899:
+            Duo = elite
+        elif rankPDuo >= 1900 and rankPDuo <= 1999:
+            Duo = master
         else:
+            Duo = grandmaster
+
+
+        if SquadPlayed <= 9:
+            rankPSquad = 0
+            Squad = '(menos de 10 partidas)'
+        elif rankPSquad <= 1399:
+            Squad = bronze
+        elif rankPSquad >= 1500 and rankPSquad <= 1599:
+            Squad = ouro
+        elif rankPSquad >= 1400 and rankPSquad <= 1499:
             Squad = prata
+        elif rankPSquad >= 1600 and rankPSquad <= 1699:
+            Squad = platinum
+        elif rankPSquad >= 1700 and rankPSquad <= 1799:
+            Squad = diamond
+        elif rankPSquad >= 1800 and rankPSquad <= 1899:
+            Squad = elite
+        elif rankPSquad >= 1900 and rankPSquad <= 1999:
+            Squad = master
+        else:
+            Squad = grandmaster
+
 
 
         EmbedPu = discord.Embed(title="PlayerUnknown's Battlegrounds\n"
-                                      "Servers SA | TPP | Season 9",
+                                      "Servers WW | TPP | New Season 1",
                                 description="----------------------\n"
                                             "Player: {}\n"
                                             "----------------------\n"
@@ -92,7 +154,8 @@ async def on_message(message):
                                             "Kills por Headshot: {}\n"
                                             "Top 10: {}\n"
                                             "Vitórias: {}\n"
-                                            "Pontos: {} {}\n"
+                                            "Pontos Max: {}\n"
+                                            "Pontos Atual: {} {}\n"
                                             "----------------------\n"
                                             "Duo\n"
                                             "Partida(s): {}\n"
@@ -102,7 +165,8 @@ async def on_message(message):
                                             "Kills por Headshot: {}\n"
                                             "Top 10: {}\n"
                                             "Vitórias: {}\n"
-                                            "Pontos: {} {}\n"
+                                            "Pontos Max: {}\n"
+                                            "Pontos Atual: {} {}\n"
                                             "----------------------\n"
                                             "Squad\n"
                                             "Partida(s): {}\n"
@@ -112,7 +176,8 @@ async def on_message(message):
                                             "Kills por Headshot: {}\n"
                                             "Top 10: {}\n"
                                             "Vitórias: {}\n"
-                                            "Pontos: {} {}\n"
+                                            "Pontos Max: {}\n"
+                                            "Pontos Atual: {} {}\n"
                                             "----------------------\n".format(
                                                         cont,
                                                         json3['roundsPlayed'],
@@ -121,7 +186,8 @@ async def on_message(message):
                                                         json3['headshotKills'],
                                                         json3['top10s'],
                                                         json3['wins'],
-                                                        PSolo4, Solo,
+                                                        MPointsSolo,
+                                                        rankPSolo, Solo,
                                                         json4['roundsPlayed'],
                                                         json4['kills'],
                                                         json4['assists'],
@@ -129,7 +195,8 @@ async def on_message(message):
                                                         json4['headshotKills'],
                                                         json4['top10s'],
                                                         json4['wins'],
-                                                        PDuo4, Duo,
+                                                        MPointsDuo,
+                                                        rankPDuo, Duo,
                                                         json5['roundsPlayed'],
                                                         json5['kills'],
                                                         json5['assists'],
@@ -137,7 +204,8 @@ async def on_message(message):
                                                         json5['headshotKills'],
                                                         json5['top10s'],
                                                         json5['wins'],
-                                                        PSquad4, Squad
+                                                        MPointsSquad,
+                                                        rankPSquad, Squad
                                                                             ))
         EmbedPu.set_author(name=message.author.name, icon_url=message.author.avatar_url)
         EmbedPu.set_footer(text='Developed by: Jacques Jacob | Integrado com API Oficial PUBG®')
@@ -152,14 +220,14 @@ async def on_message(message):
         cursor = cnx.cursor()
 
         pubg_name = cont
-        solo_points = PSolo4
-        duo_points = PDuo4
-        squad_points = PSquad4
+        solo_points = rankPSolo
+        duo_points = rankPDuo
+        squad_points = rankPSquad
 
         insert = """
                     INSERT INTO pubgdiscord (pubg_name, solo_points, duo_points, squad_points)
-                    VALUES (%s, %s, %s, %s) 
-                    ON DUPLICATE KEY UPDATE   
+                    VALUES (%s, %s, %s, %s)
+                    ON DUPLICATE KEY UPDATE
                     solo_points=%s, duo_points=%s, squad_points=%s
                  """
 
